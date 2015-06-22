0d0b5d6701c7469aa753327f14d955fa361a2815
Benjamin Roberts 6rsRBGKwBJW6Wq2HU2g3_Vrxxfo <brob695@docs.nesi.org.nz>
2015-06-23 11:52:22 +1200

    Initial files

---
+++ docserver.py
@@ -0,0 +1,18 @@
+import web
+
+from web.wsgiserver import CherryPyWSGIServer
+
+CherryPyWSGIServer.ssl_certificate = "/home/brob695/ssl_certs/docs.crt"
+CherryPyWSGIServer.ssl_private_key = "/home/brob695/ssl_certs/docs.key"
+
+urls = (
+    '/', 'index'
+)
+
+class index:
+    def GET(self):
+        return "Hello, world!"
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
